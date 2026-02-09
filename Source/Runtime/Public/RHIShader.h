3c1f3cdf503175f1041df2f36e713f0ecbfdf439
afegg555 <afegg555@gmail.com>
2026-02-10 00:23:00 +0900

    refactor: modify RHIShader from interface to class

--- Source/Runtime/Public/RHIShader.h
+++ Source/Runtime/Public/RHIShader.h
@@ -9,18 +9,66 @@ namespace won::rendering
     {
         Vertex,
         Pixel,
-        Compute,
-        All
+        Compute
     };
 
-    class WONENGINE_API RHIShader : public RHIObject
+    class WONENGINE_API RHIShader final : public RHIObject
     {
     public:
+        RHIShader() = default;
+        RHIShader(RHIShaderStage stage, const void* bytecode_data, Size bytecode_size)
+            : stage(stage)
+        {
+            SetBytecode(bytecode_data, bytecode_size);
+        }
+
         ~RHIShader() override = default;
 
-        virtual RHIShaderStage GetStage() const = 0;
-        virtual const void* GetBytecode() const = 0;
-        virtual Size GetBytecodeSize() const = 0;
+        void SetStage(RHIShaderStage new_stage)
+        {
+            stage = new_stage;
+        }
+
+        RHIShaderStage GetStage() const
+        {
+            return stage;
+        }
+
+        void SetBytecode(const void* bytecode_data, Size bytecode_size)
+        {
+            bytecode.clear();
+            if (!bytecode_data || bytecode_size == 0)
+            {
+                return;
+            }
+
+            bytecode.resize(bytecode_size);
+            std::memcpy(bytecode.data(), bytecode_data, bytecode_size);
+        }
+
+        const void* GetBytecode() const
+        {
+            return bytecode.empty() ? nullptr : bytecode.data();
+        }
+
+        Size GetBytecodeSize() const
+        {
+            return bytecode.size();
+        }
+
+        void SetName(const String& new_name) override
+        {
+            name = new_name;
+        }
+
+        const String& GetName() const override
+        {
+            return name;
+        }
+
+    private:
+        RHIShaderStage stage = RHIShaderStage::Vertex;
+        Vector<uint8> bytecode;
+        String name;
     };
 }
-
