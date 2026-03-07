bdeaf1fc2f5920a07768887226f7ee472888b925
afegg555 <afegg555@naver.com>
2026-03-07 22:28:09 +0900

    refactor: add shader offline compiler

--- Source/Tools/ShaderOfflineCompiler/main.cpp
+++ Source/Tools/ShaderOfflineCompiler/main.cpp
@@ -1,6 +1,19 @@
+#include "ShaderCompiler.h"
+#include "ShaderLibrary.h"
+#include "JobSystem.h"
+#include "Backlog.h"
 
+using namespace won;
 
 int main(int argc, char** argv)
 {
+    backlog::SetLogFile("ShaderOfflineCompileLog.log");
+    jobsystem::Initialize();
+
+    resource::ShaderCompilerOptions compiler_options;
+    compiler_options.backend = resource::ShaderCompilerBackend::DXC;
+    resource::ShaderLibrary shader_library(compiler_options);
+    shader_library.LoadAllShaders();
+
     return 0;
 }
