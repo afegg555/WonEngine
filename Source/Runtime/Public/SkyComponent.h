d3bd70a108d0adc330263dc6aaaf4a6053dba62e
afegg555 <afegg555@naver.com>
2026-04-23 16:05:12 +0900

    chore: modify default values of SkyComponent

--- Source/Runtime/Public/SkyComponent.h
+++ Source/Runtime/Public/SkyComponent.h
@@ -13,14 +13,14 @@ namespace won::ecs
 
         uint32 flags = Active;
 
-        float3 sun_direction = { 0.0f, 1.0f, 0.0f };
-        float sun_intensity = 8.0f;
+        float3 sun_direction = { 0.58f, 0.38f, 0.71f };
+        float sun_intensity = 2.0f;
 
         float3 sun_color = { 1.0f, 0.95f, 0.85f };
-        float sun_angular_radius = 0.004675f;
+        float sun_angular_radius = 0.001f;
 
-        float sun_glow_intensity = 1.0f;
-        float sun_glow_falloff = 128.0f;
+        float sun_glow_intensity = 2.91f;
+        float sun_glow_falloff = 1000.0f;
 
         float3 sky_horizon_color = { 0.75f, 0.85f, 1.0f };
         float sky_intensity = 3.0f;
