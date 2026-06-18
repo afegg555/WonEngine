081857473bb3a164a3111bc59efea5b2d6a86fa2
afegg555 <afegg555@naver.com>
2026-06-19 02:11:27 +0900

    workaround: remove cycle in system

--- Source/Runtime/Public/PhysicsUpdateSystem.h
+++ Source/Runtime/Public/PhysicsUpdateSystem.h
@@ -9,7 +9,7 @@ namespace won::ecs
     class WONENGINE_API PhysicsUpdateSystem final : public System
     {
         ComponentMask GetReadMask() const override { return transform_component_mask | collider_3d_component_mask | rigidbody_3d_component_mask; }
-        ComponentMask GetWriteMask() const override { return transform_component_mask | collider_3d_component_mask | rigidbody_3d_component_mask; }
+        ComponentMask GetWriteMask() const override { return collider_3d_component_mask | rigidbody_3d_component_mask; }
         SystemExecutionPolicy GetExecutionPolicy() const override { return SystemExecutionPolicy::Synchronous; }
 
         void Update(Scene& scene, float delta_time) override;
