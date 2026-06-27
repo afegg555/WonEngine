26c2aebe54c394d0da5cfd661371eb2c329d6038
afegg555 <afegg555@naver.com>
2026-06-27 18:14:17 +0900

    fix: fix TextUpdateSystem's write mask

--- Source/Runtime/Public/TextUpdateSystem.h
+++ Source/Runtime/Public/TextUpdateSystem.h
@@ -10,7 +10,7 @@ namespace won::ecs
     {
     public:
         ComponentMask GetReadOnlyMask() const override { return transform_component_mask | material_component_mask | layer_component_mask; }
-        ComponentMask GetWriteMask() const override { return text_2d_component_mask | text_3d_component_mask; }
+        ComponentMask GetWriteMask() const override { return text_2d_component_mask | text_3d_component_mask | sprite_2d_component_mask | sprite_3d_component_mask; }
         void Update(Scene& scene, float delta_time) override;
     };
 }
