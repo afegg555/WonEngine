9641fae26a0ff3f95354ed51e6d5950918829bbc
afegg555 <afegg555@naver.com>
2026-09-17 23:29:19 +0900

    chore: remove foliage debug draw

--- Source/Runtime/Private/FoliageSystem.cpp
+++ Source/Runtime/Private/FoliageSystem.cpp
@@ -5,27 +5,11 @@
 #include "TerrainComponent.h"
 #include "TerrainData.h"
 #include "TransformComponent.h"
-#include "DebugDraw.h"
-#include "Console.h"
-#include "MathUtils.h"
 
 namespace won::ecs
 {
-    namespace
-    {
-        static console::ConsoleVariable r_foliage_enabled("r.foliage.enabled", true, "enable foliage scatter and rendering");
-        static console::ConsoleVariable r_foliage_show("r.foliage.show", false, "draw foliage scatter points as debug crosses");
-
-        constexpr uint32 max_overlay_crosses = 20000;
-    }
-
     void FoliageSystem::Update(Scene& scene, float delta_time)
     {
-        if (!r_foliage_enabled.GetBool())
-        {
-            return;
-        }
-
         auto foliage_array = scene.GetComponentArray<FoliageComponent>().get();
         const uint32 foliage_count = static_cast<uint32>(foliage_array->GetSize());
         if (foliage_count == 0)
@@ -60,28 +44,6 @@ namespace won::ecs
                 if (foliage.IsActive() && foliage.IsDirty())
                 {
                     foliage::ScatterFoliage(foliage, *terrain_data, terrain_world);
-                }
-            }
-        }
-
-        if (r_foliage_show.GetBool())
-        {
-            const uint32 color = debugdraw::PackRGBA8(0x40, 0xff, 0x40);
-            uint32 drawn = 0;
-            for (uint32 i = 0; i < foliage_count && drawn < max_overlay_crosses; ++i)
-            {
-                const FoliageComponent& foliage = foliage_array->data[i];
-                for (const FoliageType& type : foliage.types)
-                {
-                    for (const FoliageInstance& instance : type.instances)
-                    {
-                        if (drawn >= max_overlay_crosses)
-                        {
-                            break;
-                        }
-                        debugdraw::Cross3D(instance.position, 0.25f * instance.scale, color);
-                        ++drawn;
-                    }
                 }
             }
         }
